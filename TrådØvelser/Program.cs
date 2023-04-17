c63da276745a06b51e31340d70b176688b979431
Simon Onyszczak Hansen <simo401t@zbc.dk>
2023-04-17 12:19:22 +0200

    Add project files.

---
+++ TrådØvelser/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TrådØvelser
+{
+    internal class Program
+    {
+        public void WorkThreadFunction()//Creates a method with a loop that runs 5 times
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Simple Thread");
+            }
+        }
+    }
+
+    class threprog
+    {
+        static void Main(string[] args)
+        {
+            Program pg = new Program();//Creates an object for the class above
+            Thread thread = new Thread(new ThreadStart(pg.WorkThreadFunction));//Creates a thread using the method
+            thread.Start();//runs the thread that we created
+            Console.Read();
+        }
+    }
+}
